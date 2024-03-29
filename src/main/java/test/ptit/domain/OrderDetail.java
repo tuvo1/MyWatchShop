fe35d8cba0bbe8c7a8225e56df1828f22e62cc59
tuvo1 <tulevo2510@gmail.com>
2024-03-30 00:20:32 +0700

    Watch25

---
+++ src/main/java/test/ptit/domain/OrderDetail.java
@@ -0,0 +1,45 @@
+package edu.poly.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "orderdetails")
+@Getter
+@Setter
+public class OrderDetail implements Serializable{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int orderDetailId;
+	
+	@Column(nullable = false)
+	private int quantity;
+	
+	@Column(nullable = false)
+	private double unitPrice;
+	
+	@ManyToOne
+	@JoinColumn(name = "productId")
+	private Product product;
+	
+	@ManyToOne
+	@JoinColumn(name = "orderId")
+	private Order order;
+}
